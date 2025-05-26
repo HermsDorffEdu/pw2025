5b1f30ce5b3b64ac9a1444d89685a4beaebe400f
unknown <eduardohermsdorff.ifpr@gmail.com>
2025-05-26 16:17:02 -0300

    Views para editar

--- paginas/urls.py
+++ paginas/urls.py
@@ -1,16 +1,23 @@
 from django.urls import path
 from .views import (
     Inicio,
-    ClienteCreate,
-    FotografoCreate,
-    EstudioCreate,
-    SessaoFotoCreate
+    ClienteCreate, ClienteUpdate, 
+    FotografoCreate, FotografoUpdate,
+    EstudioCreate, EstudioUpdate,
+    SessaoFotoCreate, SessaoFotoUpdate,
 )
 
 urlpatterns = [
     path("", Inicio.as_view(), name="inicio"),
-    path("adicionar/cliente/<int:pk/", ClienteCreate.as_view(), name="inserir-cliente"),
-    path("adicionar/fotografo/<int:pk/", FotografoCreate.as_view(), name="inserir-fotografo"),
-    path("adicionar/estudio/<int:pk/", EstudioCreate.as_view(), name="inserir-estudio"),
-    path("adicionar/sessao/<int:pk/", SessaoFotoCreate.as_view(), name="inserir-sessao"),
+
+    path("adicionar/cliente/", ClienteCreate.as_view(), name="inserir-cliente"),
+    path("adicionar/fotografo/", FotografoCreate.as_view(), name="inserir-fotografo"),
+    path("adicionar/estudio/", EstudioCreate.as_view(), name="inserir-estudio"),
+    path("adicionar/sessao/", SessaoFotoCreate.as_view(), name="inserir-sessao"),
+
+    path("editar/cliente/<int:pk>/", ClienteUpdate.as_view(), name="editar-cliente"),
+    path("editar/fotografo/<int:pk>/", FotografoUpdate.as_view(), name="editar-fotografo"),
+    path("editar/estudio/<int:pk>/", EstudioUpdate.as_view(), name="editar-estudio"),
+    path("editar/sessao/<int:pk>/", SessaoFotoUpdate.as_view(), name="editar-sessao"),
+
 ]
--- paginas/views.py
+++ paginas/views.py
@@ -6,13 +6,14 @@ from .models import Cliente, Fotografo, Estudio, SessaoFoto
 class Inicio(TemplateView):
     template_name = "paginas/index.html"
 
+
 class ClienteCreate(CreateView):
     model = Cliente
     fields = ['nome', 'telefone', 'email']
     template_name = 'paginas/form.html'
     success_url = reverse_lazy('inicio')
     extra_content = {'Cadastro Fotografia': 'Atualização de fotos', 'Enviar dados': 'protocolar',}
-    
+
 
 class FotografoCreate(CreateView):
     model = Fotografo
@@ -30,7 +31,40 @@ class EstudioCreate(CreateView):
 
 class SessaoFotoCreate(CreateView):
     model = SessaoFoto
-    fields = ['data', 'horario', 'tipo', 'duracao', 'cliente', 'fotografo', 'estudio']
+    fields = ['data', 'horario', 'tipo', 'duracao', 'cliente', 'fotografo', 'estudio', 'finalizado', 'valor']
+    template_name = 'paginas/form.html'
+    success_url = reverse_lazy('inicio')
+    extra_content = {'Cadastro Fotografia': 'Atualização de fotos', 'Enviar dados': 'protocolar',}
+
+
+#############################################################
+
+
+class ClienteUpdate(UpdateView):
+    model = Cliente
+    fields = ['nome', 'telefone', 'email']
+    template_name = 'paginas/form.html'
+    success_url = reverse_lazy('inicio')
+    extra_content = {'Cadastro Fotografia': 'Atualização de fotos', 'Enviar dados': 'protocolar',}
+
+
+class FotografoUpdate(UpdateView):
+    model = Fotografo
+    fields = ['nome', 'especialidade', 'telefone']
+    template_name = 'paginas/form.html'
+    success_url = reverse_lazy('inicio')
+    extra_content = {'Cadastro Fotografia': 'Atualização de fotos', 'Enviar dados': 'protocolar',}
+
+class EstudioUpdate(UpdateView):
+    model = Estudio
+    fields = ['nome', 'endereco', 'telefone']
+    template_name = 'paginas/form.html'
+    success_url = reverse_lazy('inicio')
+    extra_content = {'Cadastro Fotografia': 'Atualização de fotos', 'Enviar dados': 'protocolar',}
+
+class SessaoFotoUpdate(UpdateView):
+    model = SessaoFoto
+    fields = ['data', 'horario', 'tipo', 'duracao', 'cliente', 'fotografo', 'estudio', 'finalizado', 'valor']
     template_name = 'paginas/form.html'
     success_url = reverse_lazy('inicio')
     extra_content = {'Cadastro Fotografia': 'Atualização de fotos', 'Enviar dados': 'protocolar',}
